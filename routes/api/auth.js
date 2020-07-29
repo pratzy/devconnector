db736abae556050f9c5c93835769ad45a35a5427
Prateek Agarwal <pratzy06@gmail.com>
2020-07-30 01:26:55 +0800

    More code

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,84 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 // @route    GET api/auth
 // @desc     Test route
+// @access   Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json("Server Error");
+  }
+});
+
+// @route    POST api/auth
+// @desc     Authenticate user and get token
 // @access   Public
-router.get("/", (req, res) => res.send("Auth route"));
+router.post(
+  "/",
+  [
+    check("email", "Enter valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    console.log("Received request: ", req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if the user exists
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          return res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
--- test_requests/users/authenticate_user.js
+++ test_requests/users/authenticate_user.js
@@ -1,18 +1,16 @@
-import request from "request";
+var request = require("request");
 var options = {
   method: "POST",
-  url: "http://localhost:5000/api/users",
+  url: "http://localhost:5000/api/auth",
   headers: {
     "Content-Type": "application/json",
   },
   body: JSON.stringify({
-    name: "Prateek Agarwal",
     email: "mail.prateek.agarwal@gmail.com",
     password: "123456",
   }),
 };
-
-request(options, (error, response) => {
+request(options, function (error, response) {
   if (error) throw new Error(error);
   console.log(response.body);
 });
